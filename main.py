a247867a5c8d04fff0fcd8f387e1de5d0f5cdfae
caishiqing <1349053218@qq.com>
2023-02-04 22:20:37 +0800

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from model import RenderNet
 from renderer import RENDERER_FACTORY
+from loss import PixelLoss
 import tensorflow as tf
 import fire
 import os
@@ -15,11 +16,14 @@ def train_renderer(canvas_width: int = 128,
     dataset = renderer.generate_dataset(train_args.pop('batch_size', 64))
     model = RenderNet(renderer.param_size, canvas_width)
     model.build(optimizer=tf.keras.optimizers.Adam(train_args.pop('learning_rate'), 1e-3),
-                loss=tf.keras.losses.mae)
+                loss=PixelLoss(
+                    power=train_args.pop('power', 1),
+                    ignore_color=train_args.pop('ignore_color', False)
+    ))
 
     if save_path is not None:
         checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=save_path,
-                                                        monitor='val_loss',
+                                                        monitor='loss',
                                                         save_best_only=True,
                                                         save_weights_only=False)
         callbacks = [checkpoint]
