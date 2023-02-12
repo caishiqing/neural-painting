08624cd79c0ec1bf386d173a46679991a7c87221
caishiqing <1349053218@qq.com>
2023-02-12 20:48:02 +0800

    .

--- model.py
+++ model.py
@@ -3,6 +3,67 @@ import tensorflow as tf
 import types
 
 
+# def RenderNet(param_size, canvas_width):
+#     fmap_size = canvas_width // 8
+#     model = tf.keras.Sequential(
+#         layers=[
+#             layers.Input((param_size,), dtype=tf.float32),
+#             layers.Dense(512, activation='relu'),
+#             layers.Dense(1024, activation='relu'),
+#             layers.Dense(2048, activation='relu'),
+#             layers.Dense(fmap_size * fmap_size * 12),
+#             layers.BatchNormalization(),
+#             layers.Activation('relu'),
+#             layers.Reshape([canvas_width//4, canvas_width//4, 3]),
+#             layers.Conv2DTranspose(8, 3, 2, padding='same', activation='relu'),
+#             layers.Conv2DTranspose(3, 3, 2, padding='same'),
+#             layers.BatchNormalization(),
+#             layers.Activation('sigmoid')
+#         ]
+#     )
+#     model.save = types.MethodType(save_model, model)
+#     return model
+
+
+class PixelShuffle(layers.Layer):
+    def __init__(self, upscale_factor, **kwargs):
+        super(PixelShuffle, self).__init__(**kwargs)
+        self.upscale_factor = upscale_factor
+
+    def get_config(self):
+        config = super(PixelShuffle, self).get_config()
+        config['upscale_factor'] = self.upscale_factor
+
+    def build(self, input_shape):
+        b, h, w, c = input_shape
+        assert (c // self.upscale_factor) % self.upscale_factor == 0
+        self.output_channels = c // self.upscale_factor // self.upscale_factor
+        super(PixelShuffle, self).build(input_shape)
+
+    def _phase_shift(self, x):
+        b, h, w, _ = x.get_shape().as_list()
+        r = self.upscale_factor
+        x = tf.reshape(x, (-1, h, w, r, r))
+        x = tf.transpose(x, (0, 1, 2, 4, 3))
+        x = tf.split(x, h, 1)  # h, [b, 1, w, r, r]
+        x = tf.concat(x, axis=3)  # b, 1, w, h*r, r
+        x = tf.squeeze(x, 1)  # b, w, h*r, r
+        x = tf.split(x, w, 1)  # w, [b, 1, h*r, r]
+        x = tf.concat(x, axis=3)  # b, 1, h*r, w*r
+        x = tf.squeeze(x, 1)  # b, h*r, w*r
+        return tf.expand_dims(x, -1)
+
+    def call(self, inputs):
+        xs = tf.split(inputs, self.output_channels, -1)
+        xs = [self._phase_shift(x) for x in xs]
+        return tf.concat(xs, axis=-1)
+
+    def compute_output_shape(self, input_shape):
+        b, h, w, c = input_shape
+        r = self.upscale_factor
+        return b, h*r, w*r, c//(r**2)
+
+
 def RenderNet(param_size, canvas_width):
     fmap_size = canvas_width // 8
     model = tf.keras.Sequential(
@@ -11,12 +72,19 @@ def RenderNet(param_size, canvas_width):
             layers.Dense(512, activation='relu'),
             layers.Dense(1024, activation='relu'),
             layers.Dense(2048, activation='relu'),
-            layers.Dense(fmap_size * fmap_size * 12),
+            layers.Dense(fmap_size * fmap_size * 16),
             layers.BatchNormalization(),
             layers.Activation('relu'),
-            layers.Reshape([canvas_width//4, canvas_width//4, 3]),
-            layers.Conv2DTranspose(8, 3, 2, padding='same', activation='relu'),
-            layers.Conv2DTranspose(3, 3, 2, padding='same'),
+            layers.Reshape([fmap_size, fmap_size, 16]),
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.Conv2D(32, 3, padding='same'),
+            PixelShuffle(2),
+            layers.Conv2D(16, 3, padding='same', activation='relu'),
+            layers.Conv2D(16, 3, padding='same'),
+            PixelShuffle(2),
+            layers.Conv2D(8, 3, padding='same', activation='relu'),
+            layers.Conv2D(4*3, 3, padding='same'),
+            PixelShuffle(2),
             layers.BatchNormalization(),
             layers.Activation('sigmoid')
         ]
