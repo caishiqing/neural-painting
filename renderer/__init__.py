45d5f5eeecd292905e8ad03351588b4429ade026
caishiqing <caishiqing@tom.com>
2023-12-17 11:47:55 +0800

    .

--- renderer/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import Renderer
-from .oilbrush import OilBrush
-from .watercolor import WaterColor
-
-RENDERER_FACTORY = {
-    'renderer': Renderer,
-    'oilbrush': OilBrush,
-    'watercolor': WaterColor
-}
